b3fd27fc3c2f8a69beb3197f1c1ca1fc022313f7
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-23 11:33:26 +0530

    LC9

---
+++ Solutions/com/src/main/leetcode/LC9.java
@@ -0,0 +1,42 @@
+package com.src.main.leetcode;
+
+/**
+ * 9. Palindrome Number
+ */
+
+public class LC9 {
+
+    static class Solution {
+
+        public boolean isPalindrome(int x) {
+
+            if (x < 0) return false;
+
+            int original = x;
+            long reversed = 0;   // long to avoid overflow
+
+            while (x > 0) {
+                int digit = x % 10;
+                reversed = reversed * 10 + digit;
+                x /= 10;
+            }
+
+            return original == reversed;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int num1 = 121;
+        int num2 = -121;
+        int num3 = 10;
+        int num4 = 1221;
+
+        System.out.println(num1 + " is palindrome? " + solution.isPalindrome(num1)); // true
+        System.out.println(num2 + " is palindrome? " + solution.isPalindrome(num2)); // false
+        System.out.println(num3 + " is palindrome? " + solution.isPalindrome(num3)); // false
+        System.out.println(num4 + " is palindrome? " + solution.isPalindrome(num4)); // true
+    }
+}
