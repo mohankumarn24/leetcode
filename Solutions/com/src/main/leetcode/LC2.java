c433ed95c73eadb2b29cbbdb727ab07080040717
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-23 11:25:56 +0530

    LC 2

---
+++ Solutions/com/src/main/leetcode/LC2.java
@@ -0,0 +1,64 @@
+package com.src.main.leetcode;
+
+/**
+ * 2. Add Two Numbers
+ */
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode temp = new ListNode(0);
+        ListNode cur = temp;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry > 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+        }
+
+        return temp.next;
+    }
+}
+
+public class LC2 {
+
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        // Example: l1 = [2,4,3], l2 = [5,6,4]
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        ListNode result = sol.addTwoNumbers(l1, l2);
+
+        // Print the result
+        while (result != null) {
+            System.out.print(result.val);
+            if (result.next != null) System.out.print(" -> ");
+            result = result.next;
+        }
+    }
+}
