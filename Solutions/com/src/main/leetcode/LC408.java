3c6f31d3aa8044b1390f32abde94c16ad65db9fa
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-23 10:15:57 +0530

    LC408

---
+++ Solutions/com/src/main/leetcode/LC408.java
@@ -0,0 +1,45 @@
+package com.src.main.leetcode;
+
+/**
+ * 408. Valid Word Abbreviation
+ */
+class LC408 {
+
+    public boolean validWordAbbreviation(String word, String abbr) {
+
+        int i = 0; // pointer for word
+        int j = 0; // pointer for abbr
+
+        while (i < word.length() && j < abbr.length()) {
+            char c = abbr.charAt(j);
+
+            if (Character.isDigit(c)) {
+                if (c == '0') return false; // leading zero invalid
+
+                int start = j;
+                while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    j++;
+                }
+                int num = Integer.parseInt(abbr.substring(start, j));
+                i += num;
+            } else {
+                if (word.charAt(i) != c) return false;
+                i++;
+                j++;
+            }
+        }
+
+        return i == word.length() && j == abbr.length();
+    }
+
+    public static void main(String[] args) {
+        LC408 sol = new LC408();
+
+        System.out.println(sol.validWordAbbreviation("sahan", "s3n")); // true
+        //System.out.println(sol.validWordAbbreviation("internationalization", "i12iz4n")); // true
+        //System.out.println(sol.validWordAbbreviation("apple", "a2e")); // false
+        //System.out.println(sol.validWordAbbreviation("substitution", "s10n")); // true
+        //System.out.println(sol.validWordAbbreviation("word", "w02d")); // false
+    }
+}
+
